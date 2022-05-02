002abedec39340096302fae60b8231290eecbc91
ImSirTea <45457205+ImSirTea@users.noreply.github.com>
2022-05-02 03:05:54 +0100

    Tests setting options works

--- tests/unit/__tests__/components/grid/gridConfiguration.spec.ts
+++ tests/unit/__tests__/components/grid/gridConfiguration.spec.ts
@@ -15,12 +15,18 @@ describe("GridConfiguration", () => {
 
   it("Adds a text and number column", () => {
     // Adding a text column
-    gridConfiguration.addTextColumn("Text", (item) => item.first);
+    gridConfiguration.addTextColumn("first", (item) => item.first);
     expect(gridConfiguration.columns).toHaveLength(1);
 
     // Adding a number column
-    gridConfiguration.addNumberColumn("Number", (item) => item.index);
+    gridConfiguration.addNumberColumn("index", (item) => item.index);
     expect(gridConfiguration.columns).toHaveLength(2);
+
+    // Adding options works as expected
+    const lastNameColumn = gridConfiguration
+      .addTextColumn("last", (item) => item.last)
+      .setOptions({ defaultPin: "right" });
+    expect(lastNameColumn.options.defaultPin).toEqual("right");
   });
 
   it("Adds an action column", () => {
@@ -38,7 +44,7 @@ describe("GridConfiguration", () => {
     expect(actionColumn.actions[1]).not.toHaveProperty("onClick");
 
     // Expect action column to be last column
-    gridConfiguration.addNumberColumn("Number", (item) => item.index);
+    gridConfiguration.addNumberColumn("index", (item) => item.index);
     expect(
       gridConfiguration.columns[gridConfiguration.columns.length - 1]
     ).toBe(actionColumn);
@@ -57,7 +63,7 @@ describe("GridConfiguration", () => {
 
   it("Adds a selection column", () => {
     // Used to test adding select after still puts it first
-    gridConfiguration.addNumberColumn("Number", (item) => item.index);
+    gridConfiguration.addNumberColumn("index", (item) => item.index);
 
     // Adding an action column
     const selectColumn = gridConfiguration.withSelectColumn();
